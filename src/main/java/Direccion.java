1d984502cd03ca572c29992df8492babe1046608
Galina K. <galinakatulskaja1228@list.ru>
2018-11-08 15:28:11 +0100

    Enum Direccion: ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA y IZQUIERDA_ABAJO

---
+++ src/main/java/Direccion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Galina
+ */
+public enum Direccion {
+    //Crea un enumerado llamado Direccion, dentro del paquete adecuado, que contenga los valores:
+    ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO;
+}
