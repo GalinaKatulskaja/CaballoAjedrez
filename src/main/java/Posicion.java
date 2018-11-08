eaf12be74df6dd3f55b987bd0ac3beaf713ee21b
Galina K. <galinakatulskaja1228@list.ru>
2018-11-08 15:32:28 +0100

    Clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada.

---
+++ src/main/java/Posicion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Galina
+ */
+public class Posicion {
+    //Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada.
+    private int fila;
+    private char columna;
+    
+}
